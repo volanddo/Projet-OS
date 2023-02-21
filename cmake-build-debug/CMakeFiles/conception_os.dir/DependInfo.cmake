899b9957f77e748efeb339551b226e76191fa048
Dorian Voland <dorian.voland@gmail.com>
2023-02-21 13:03:44 +0100

    add all

---
+++ cmake-build-debug/CMakeFiles/conception_os.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/clock_print.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/clock_print.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/console.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/console.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/debugger.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/debugger.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/div64.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/div64.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/doprnt.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/doprnt.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/empty.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/empty.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/panic.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/panic.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/printf.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/printf.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/processor_structs.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/processor_structs.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/processus.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/processus.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/sprintf.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/sprintf.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/start.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/start.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/string.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/string.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/strtol.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/strtol.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/strtoul.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/strtoul.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/tinyalloc.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/tinyalloc.c.o"
+  "/user/7/volandd/Documents/2A/conception os/src_de_base/traitants_.c" "/user/7/volandd/Documents/2A/conception os/cmake-build-debug/CMakeFiles/conception_os.dir/src_de_base/traitants_.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src_de_base"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/conception_os.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/conception_os.dir/src_de_base/clock_print.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/console.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/debugger.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/div64.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/doprnt.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/empty.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/panic.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/printf.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/processor_structs.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/processus.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/sprintf.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/start.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/string.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/strtol.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/strtoul.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/tinyalloc.c.o"
+  "CMakeFiles/conception_os.dir/src_de_base/traitants_.c.o"
+  "conception_os"
+  "conception_os.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/conception_os.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
